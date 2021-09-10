2b7138d35eb0584ee21d2fed9b7f086bbd294d40
nour-gamal <nmg181963@hotmail.com>
2021-09-10 19:57:23 +0200

    packageJson

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require("./db/mongoose");
 const Task = require("./models/Task");
 const User = require("./models/User");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
