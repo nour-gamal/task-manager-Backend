d335dec9bce23133928fb88dbbf64735f8b9d645
nour-gamal <nmg181963@hotmail.com>
2021-09-12 21:00:49 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -7,24 +7,30 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.post("/adduser", (req, res) => {
+app.post("/adduser", async (req, res) => {
 	const user = new User(req.body);
-	user
-		.save()
-		.then(() => {
-			res.status(201).send(user);
-		})
-		.catch((err) => {
-			res.status(400).send(err);
-		});
+	try {
+		await user.save();
+		res.status(201).send({ status: 201, user });
+	} catch (e) {
+		res.status(400).send({ status: 400, error: e });
+	}
 });
 
-app.get("/getAllUsers", (req, res) => {
-	User.find({})
-		.then((User) => res.send(User))
-		.catch((e) => res.status(500).send(e));
+app.get("/getAllUsers", async (req, res) => {
+	try {
+		const users = await User.find({});
+
+		res.status(200).send({
+			status: 200,
+			users,
+		});
+	} catch (e) {
+		res.status(401).send(e);
+	}
 });
-app.get("/getUserById", (req, res) => {
+
+app.get("/getUserById", async (req, res) => {
 	const id = req.query.id;
 	if (!id) {
 		return res.status(400).send({
@@ -32,32 +38,42 @@ app.get("/getUserById", (req, res) => {
 			status: 400,
 		});
 	}
-	User.findById(id)
-		.then((user) => {
-			if (!user) {
-				return res.status(404).send({
-					message: "User not found",
-					status: 404,
-				});
-			}
-
-			res.send(user);
-		})
-		.catch((e) => res.status(500).send(e));
+	try {
+		const user = await User.findById(id);
+		if (!user) {
+			return res.status(404).send({
+				status: 404,
+				error: "User not found!",
+			});
+		}
+		res.status(201).send({
+			status: 201,
+			user,
+		});
+	} catch (e) {
+		res.status(500).send({
+			status: 500,
+			error: e,
+		});
+	}
 });
 
-app.post("/addTask", (req, res) => {
+app.post("/addTask", async (req, res) => {
 	const task = new Task(req.body);
 
-	task
-		.save()
-		.then(() => {
-			res.status(201).send(task);
-		})
-		.catch((e) => res.status(400).send(e));
+	try {
+		const newTask = await task.save();
+
+		res.status(201).send({ status: 201, message: "Task Added", task: newTask });
+	} catch (e) {
+		res.status(400).status({
+			status: 400,
+			error: e,
+		});
+	}
 });
 
-app.get("/getTaskById", (req, res) => {
+app.get("/getTaskById", async (req, res) => {
 	const id = req.query.id;
 	if (!id) {
 		return res.status(404).send({
@@ -65,19 +81,17 @@ app.get("/getTaskById", (req, res) => {
 			status: 404,
 		});
 	}
-	Task.findById(id)
-		.then((Task) => {
-			res.status(200).send({
-				status: 401,
-				data: Task,
-			});
-		})
-		.catch((err) => {
-			res.status(401).send({
-				status: 401,
-				error: err,
-			});
+
+	try {
+		const task = await Task.findById(id);
+
+		res.status(200).send({ status: 200, data: task });
+	} catch (e) {
+		res.status(500).send({
+			status: 500,
+			e: e,
 		});
+	}
 });
 
 app.get("/getAllTasks", (req, res) => {
@@ -96,29 +110,28 @@ app.get("/getAllTasks", (req, res) => {
 		});
 });
 
-app.get("/removeReturnUncompleteTasks", (req, res) => {
+app.get("/removeReturnUncompleteTasks", async (req, res) => {
 	const id = req.query.id;
 	if (!id) {
 		return res.status(400).send({
 			message: "id property missing",
 		});
 	}
-	Task.findByIdAndDelete(id)
-		.then((task) => {
-			if (!task) {
-				return res.status(404).send({ status: 404, message: "Task not found" });
-			}
-			return Task.countDocuments({ completed: false });
-		})
-		.then((task) => {
-			res.status(201).send({ status: 201, uncompletedTasksCount: task });
-		})
-		.catch((e) => {
-			res.status(401).send({ status: 401, error: e });
+	try {
+		const removedTask = await Task.findByIdAndDelete(id);
+		const uncompletedTasks = await Task.find({ completed: false });
+		res.status(200).send({
+			status: 200,
+			data: uncompletedTasks,
 		});
+	} catch (e) {
+		res.status(404).send({
+			status: 404,
+			error: e,
+		});
+	}
 });
 
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
-
